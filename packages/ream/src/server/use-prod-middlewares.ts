344720e80a7b2e19a70cfe8098c18428029163be
Kevin Titor <0x142857@gmail.com>
2020-04-13 01:29:46 +0800

    Delete use-prod-middlewares.ts

--- packages/ream/src/server/use-prod-middlewares.ts
+++
@@ -1,6 +0,0 @@
-import { Ream } from "..";
-import express, {Express} from 'express'
-
-export function useProdMiddlewares(api: Ream, server: Express) {
-  server.use('/_ream', express.static(api.resolveDotReam('client')))
-}
