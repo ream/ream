327657fac1ed602c4938212c07ae70d20da7fd0d
EGOIST <0x142857@gmail.com>
2020-04-13 23:28:33 +0800

    Fix vue-meta config

--- packages/ream-server/src/use-meta.ts
+++ packages/ream-server/src/use-meta.ts
@@ -4,8 +4,8 @@ import Meta from 'vue-meta'
 export function useMeta() {
   Vue.use(Meta, {
     keyName: 'head',
-    attribute: 'x-ream-head',
-    ssrAttribute: 'x-ream-ssr',
+    attribute: 'data-ream-head',
+    ssrAttribute: 'data-ream-ssr',
     tagIDKeyName: 'rhid',
     refreshOnceOnNavigation: true,
   })
