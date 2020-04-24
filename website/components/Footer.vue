7bd7419ebf9c2b114de1dff3f06a1349e3e45027
EGOIST <0x142857@gmail.com>
2020-04-24 23:30:06 +0800

    chore(website): add vercel logo

--- website/components/Footer.vue
+++ website/components/Footer.vue
@@ -1,7 +1,20 @@
 <template>
-  <footer class="mt-12">
-    <div class="container mx-auto h-24 flex items-center text-gray-700">
-      &copy; 2020 Created by EGOIST (Kevin Titor).
+  <footer class="mt-12 border-t pt-10 border-gray-200">
+    <div
+      class="container mx-auto flex items-center text-gray-700"
+    >&copy; 2020 Created by EGOIST (Kevin Titor).</div>
+    <div class="container mx-auto mt-5 mb-12">
+      <a href="https://vercel.com" rel="nofollow noreferer" target="_blank"><Vercel /></a>
     </div>
   </footer>
 </template>
+
+<script>
+import Vercel from '@/components/Vercel.vue'
+
+export default {
+  components: {
+    Vercel,
+  },
+}
+</script>
