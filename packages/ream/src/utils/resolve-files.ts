f77416d328a1e0b0190efea44bc6e3bcfa072c0b
EGOIST <0x142857@gmail.com>
2021-03-04 21:26:26 +0800

    refactor: remove unused files

--- packages/ream/src/utils/resolve-files.ts
+++
@@ -1,7 +0,0 @@
-import JoyCon from 'joycon'
-import { dirname } from 'path'
-
-export function resolveFiles(files: string[], cwd: string) {
-  const joycon = new JoyCon()
-  return joycon.resolve(files, cwd, dirname(cwd))
-}
