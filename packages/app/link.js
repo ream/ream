f38419b455fc856711fd087271f773e11241d009
EGOIST <0x142857@gmail.com>
2021-03-12 23:19:45 +0800

    fix: support prefetching static preload in safari

--- packages/app/link.js
+++ packages/app/link.js
@@ -2,7 +2,28 @@ import { defineComponent } from 'vue'
 import { RouterLink as VueRouterLink } from 'vue-router'
 import { getPreloadPath } from './lib/runtime-utils'
 
-const prefetchedLinks = new Set()
+const prefetchedPaths = new Set()
+
+const link = import.meta.env.SSR ? null : document.createElement('link')
+const hasPrefetch = import.meta.env.SSR
+  ? false
+  : link.relList && link.relList.supports && link.relList.supports('prefetch')
+
+/**
+ * Fetch URL using `<link rel="prefetch">` with fallback to `fetch` API
+ * Safari doesn't have it enabled by default: https://caniuse.com/?search=prefetch
+ * @param {string} url
+ */
+const prefetchURL = (url) => {
+  if (hasPrefetch) {
+    const link = document.createElement('link')
+    link.rel = 'prefetch'
+    link.href = url
+    document.head.appendChild(link)
+  } else {
+    fetch(url, { credentials: `include` })
+  }
+}
 
 export const RouterLink = defineComponent({
   ...VueRouterLink,
@@ -10,9 +31,12 @@ export const RouterLink = defineComponent({
   name: 'RouterLink',
 
   async mounted() {
-    if (import.meta.env.DEV || prefetchedLinks.has(this.to)) return
+    if (import.meta.env.DEV) return
 
     const { matched, path } = this.$router.resolve(this.to)
+
+    if (prefetchedPaths.has(path)) return
+
     const components = await Promise.all(
       matched.map((m) =>
         typeof m.components.default === 'function'
@@ -22,11 +46,9 @@ export const RouterLink = defineComponent({
     )
     for (const component of components) {
       if (component.$$staticPreload) {
-        const link = document.createElement('link')
-        link.rel = 'prefetch'
-        link.href = getPreloadPath(path)
-        document.head.appendChild(link)
-        prefetchedLinks.add(this.to)
+        const url = getPreloadPath(path)
+        prefetchURL(url)
+        prefetchedPaths.add(path)
         break
       }
     }
