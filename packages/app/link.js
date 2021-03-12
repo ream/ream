68e6085605fd78db2c7108b6a7659ce04b47655f
EGOIST <0x142857@gmail.com>
2021-03-13 00:30:44 +0800

    Revert "refactor: use fetch to prefetch url"
    This reverts commit e6f214d84a8546057af7bdc16424621a9249e8d8.

--- packages/app/link.js
+++ packages/app/link.js
@@ -4,6 +4,27 @@ import { getPreloadPath } from './lib/runtime-utils'
 
 const prefetchedPaths = new Set()
 
+const link = import.meta.env.SSR ? null : document.createElement('link')
+const hasPrefetch = import.meta.env.SSR
+  ? false
+  : link.relList && link.relList.supports && link.relList.supports('prefetch')
+
+/**
+ * Fetch URL using `<link rel="prefetch">` with fallback to `fetch` API
+ * Safari doesn't have it enabled by default: https://caniuse.com/?search=prefetch
+ * @param {string} url
+ */
+const prefetchURL = (url) => {
+  if (hasPrefetch) {
+    const link = document.createElement('link')
+    link.rel = 'prefetch'
+    link.href = url
+    document.head.appendChild(link)
+  } else {
+    fetch(url, { credentials: `include` })
+  }
+}
+
 export const RouterLink = defineComponent({
   ...VueRouterLink,
 
@@ -26,7 +47,7 @@ export const RouterLink = defineComponent({
     for (const component of components) {
       if (component.$$staticPreload) {
         const url = getPreloadPath(path)
-        fetch(url, { credentials: `include` })
+        prefetchURL(url)
         prefetchedPaths.add(path)
         break
       }
