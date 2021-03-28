78d963ed14e7729c63de6b5dfb73eef6a11bb173
EGOIST <0x142857@gmail.com>
2021-03-29 00:25:45 +0800

    test: use real fs

--- packages/ream/src/node/utils/load-routes.spec.ts
+++ packages/ream/src/node/utils/load-routes.spec.ts
@@ -1,314 +1,299 @@
 import path from 'path'
-import { Volume, createFsFromVolume } from 'memfs'
+import os from 'os'
+import fs from 'fs'
 import { createRoutesLoader } from './load-routes'
+import { normalizePath } from './normalize-path'
 
-const prepareFs = (files: string[]) => {
-  const mfs = createFsFromVolume(new Volume())
-  files.map((file) => {
-    mfs.mkdirpSync(path.dirname(file))
-    mfs.writeFileSync(file, '', { encoding: 'utf8' })
+const random = () => Math.random().toString(36).substring(7)
+
+const loadRoutes = (files: string[]) => {
+  const dirname = path.join(os.tmpdir(), `ream-${random()}`)
+
+  files.forEach((file) => {
+    const outpath = path.join(dirname, file)
+    fs.mkdirSync(path.dirname(outpath), { recursive: true })
+    fs.writeFileSync(outpath, '', 'utf8')
   })
-  return {
-    readdirSync: (dir: string) =>
-      mfs.readdirSync(dir, { encoding: 'utf8' }) as string[],
-    statSync: (filepath: string) => mfs.statSync(filepath),
-  }
+
+  const dir = path.resolve(dirname)
+  const loader = createRoutesLoader(dir)
+  const result = loader.load()
+
+  return JSON.stringify(result, null, 2).replace(
+    new RegExp(normalizePath(dir), 'g'),
+    ''
+  )
 }
 
 test('simple routes', () => {
-  const loader = createRoutesLoader(
-    '/o',
-    prepareFs([
-      '/o/index.vue',
-      '/o/about.vue',
-      '/o/profile.vue',
-      '/o/hello.ts',
-      '/o/graphql.js',
-      '/o/api/posts.js',
-    ])
-  )
-  const result = loader.load()
+  const result = loadRoutes([
+    'index.vue',
+    'about.vue',
+    'profile.vue',
+    'hello.ts',
+    'graphql.js',
+    'api/posts.js',
+  ])
   expect(result).toMatchInlineSnapshot(`
-    Object {
-      "endpoints": Array [
-        Object {
-          "file": "/o/api/posts.js",
-          "isEndpoint": true,
-          "name": "api/posts",
-          "path": "/api/posts",
+    "{
+      \\"pages\\": [
+        {
+          \\"name\\": \\"about\\",
+          \\"path\\": \\"/about\\",
+          \\"isEndpoint\\": false,
+          \\"file\\": \\"/about.vue\\"
         },
-        Object {
-          "file": "/o/graphql.js",
-          "isEndpoint": true,
-          "name": "graphql",
-          "path": "/graphql",
+        {
+          \\"name\\": \\"index\\",
+          \\"path\\": \\"/\\",
+          \\"isEndpoint\\": false,
+          \\"file\\": \\"/index.vue\\"
         },
-        Object {
-          "file": "/o/hello.ts",
-          "isEndpoint": true,
-          "name": "hello",
-          "path": "/hello",
-        },
+        {
+          \\"name\\": \\"profile\\",
+          \\"path\\": \\"/profile\\",
+          \\"isEndpoint\\": false,
+          \\"file\\": \\"/profile.vue\\"
+        }
       ],
-      "errorFile": undefined,
-      "notFoundFile": undefined,
-      "pages": Array [
-        Object {
-          "file": "/o/about.vue",
-          "isEndpoint": false,
-          "name": "about",
-          "path": "/about",
+      \\"endpoints\\": [
+        {
+          \\"name\\": \\"api/posts\\",
+          \\"path\\": \\"/api/posts\\",
+          \\"isEndpoint\\": true,
+          \\"file\\": \\"/api/posts.js\\"
         },
-        Object {
-          "file": "/o/index.vue",
-          "isEndpoint": false,
-          "name": "index",
-          "path": "/",
+        {
+          \\"name\\": \\"graphql\\",
+          \\"path\\": \\"/graphql\\",
+          \\"isEndpoint\\": true,
+          \\"file\\": \\"/graphql.js\\"
         },
-        Object {
-          "file": "/o/profile.vue",
-          "isEndpoint": false,
-          "name": "profile",
-          "path": "/profile",
-        },
-      ],
-    }
+        {
+          \\"name\\": \\"hello\\",
+          \\"path\\": \\"/hello\\",
+          \\"isEndpoint\\": true,
+          \\"file\\": \\"/hello.ts\\"
+        }
+      ]
+    }"
   `)
 })
 
 test('error and 404 page', () => {
-  const loader = createRoutesLoader(
-    '/o',
-    prepareFs(['/o/index.vue', '/o/404.vue', '/o/_error.vue'])
-  )
-  const result = loader.load()
+  const result = loadRoutes(['index.vue', '404.vue', '_error.vue'])
   expect(result).toMatchInlineSnapshot(`
-    Object {
-      "endpoints": Array [],
-      "errorFile": "/o/_error.vue",
-      "notFoundFile": "/o/404.vue",
-      "pages": Array [
-        Object {
-          "file": "/o/index.vue",
-          "isEndpoint": false,
-          "name": "index",
-          "path": "/",
-        },
+    "{
+      \\"errorFile\\": \\"/_error.vue\\",
+      \\"notFoundFile\\": \\"/404.vue\\",
+      \\"pages\\": [
+        {
+          \\"name\\": \\"index\\",
+          \\"path\\": \\"/\\",
+          \\"isEndpoint\\": false,
+          \\"file\\": \\"/index.vue\\"
+        }
       ],
-    }
+      \\"endpoints\\": []
+    }"
   `)
 })
 
 test('ignore files starting with underscore', () => {
-  const loader = createRoutesLoader(
-    '/o',
-    prepareFs(['/o/_foo.vue', '/o/foo/bar/_baz.ts'])
-  )
-  const result = loader.load()
+  const result = loadRoutes(['_foo.vue', 'foo/bar/_baz.ts'])
   expect(result).toMatchInlineSnapshot(`
-    Object {
-      "endpoints": Array [],
-      "errorFile": undefined,
-      "notFoundFile": undefined,
-      "pages": Array [],
-    }
+    "{
+      \\"pages\\": [],
+      \\"endpoints\\": []
+    }"
   `)
 })
 
 describe('layout', () => {
   it('handles layout in root folder', () => {
-    const loader = createRoutesLoader(
-      '/o',
-      prepareFs([
-        '/o/index.vue',
-        '/o/about.vue',
-        '/o/profile.vue',
-        '/o/_layout.vue',
-      ])
-    )
-    const result = loader.load()
+    const result = loadRoutes([
+      'index.vue',
+      'about.vue',
+      'profile.vue',
+      '_layout.vue',
+    ])
     expect(result).toMatchInlineSnapshot(`
-      Object {
-        "endpoints": Array [],
-        "errorFile": undefined,
-        "notFoundFile": undefined,
-        "pages": Array [
-          Object {
-            "children": Array [
-              Object {
-                "file": "/o/about.vue",
-                "isEndpoint": false,
-                "name": "about",
-                "path": "about",
+      "{
+        \\"pages\\": [
+          {
+            \\"name\\": \\"_layout\\",
+            \\"path\\": \\"/\\",
+            \\"isEndpoint\\": false,
+            \\"file\\": \\"/_layout.vue\\",
+            \\"children\\": [
+              {
+                \\"name\\": \\"about\\",
+                \\"path\\": \\"about\\",
+                \\"isEndpoint\\": false,
+                \\"file\\": \\"/about.vue\\"
               },
-              Object {
-                "file": "/o/index.vue",
-                "isEndpoint": false,
-                "name": "index",
-                "path": "",
+              {
+                \\"name\\": \\"index\\",
+                \\"path\\": \\"\\",
+                \\"isEndpoint\\": false,
+                \\"file\\": \\"/index.vue\\"
               },
-              Object {
-                "file": "/o/profile.vue",
-                "isEndpoint": false,
-                "name": "profile",
-                "path": "profile",
-              },
-            ],
-            "file": "/o/_layout.vue",
-            "isEndpoint": false,
-            "name": "_layout",
-            "path": "/",
-          },
+              {
+                \\"name\\": \\"profile\\",
+                \\"path\\": \\"profile\\",
+                \\"isEndpoint\\": false,
+                \\"file\\": \\"/profile.vue\\"
+              }
+            ]
+          }
         ],
-      }
+        \\"endpoints\\": []
+      }"
     `)
   })
 
   it('handles layout in nested routes', () => {
-    const result = createRoutesLoader(
-      '/o',
-      prepareFs([
-        '/o/index.vue',
-        '/o/about.vue',
-        '/o/foo/bar.vue',
-        '/o/foo/baz.vue',
-        '/o/foo/_layout.vue',
-        '/o/foo/bar2/a.vue',
-        '/o/foo/bar2/b.vue',
-        '/o/foo/bar2/c.vue',
-        '/o/foo/bar2/_layout.vue',
-      ])
-    ).load()
+    const result = loadRoutes([
+      'index.vue',
+      'about.vue',
+      'foo/bar.vue',
+      'foo/baz.vue',
+      'foo/_layout.vue',
+      'foo/bar2/a.vue',
+      'foo/bar2/b.vue',
+      'foo/bar2/c.vue',
+      'foo/bar2/index.vue',
+      'foo/bar2/_layout.vue',
+    ])
     expect(result).toMatchInlineSnapshot(`
-      Object {
-        "endpoints": Array [],
-        "errorFile": undefined,
-        "notFoundFile": undefined,
-        "pages": Array [
-          Object {
-            "file": "/o/about.vue",
-            "isEndpoint": false,
-            "name": "about",
-            "path": "/about",
+      "{
+        \\"pages\\": [
+          {
+            \\"name\\": \\"about\\",
+            \\"path\\": \\"/about\\",
+            \\"isEndpoint\\": false,
+            \\"file\\": \\"/about.vue\\"
           },
-          Object {
-            "children": Array [
-              Object {
-                "file": "/o/foo/bar.vue",
-                "isEndpoint": false,
-                "name": "foo/bar",
-                "path": "bar",
+          {
+            \\"name\\": \\"foo/_layout\\",
+            \\"path\\": \\"/foo\\",
+            \\"isEndpoint\\": false,
+            \\"file\\": \\"/foo/_layout.vue\\",
+            \\"children\\": [
+              {
+                \\"name\\": \\"foo/bar\\",
+                \\"path\\": \\"bar\\",
+                \\"isEndpoint\\": false,
+                \\"file\\": \\"/foo/bar.vue\\"
               },
-              Object {
-                "children": Array [
-                  Object {
-                    "file": "/o/foo/bar2/a.vue",
-                    "isEndpoint": false,
-                    "name": "foo/bar2/a",
-                    "path": "a",
+              {
+                \\"name\\": \\"foo/bar2/_layout\\",
+                \\"path\\": \\"bar2\\",
+                \\"isEndpoint\\": false,
+                \\"file\\": \\"/foo/bar2/_layout.vue\\",
+                \\"children\\": [
+                  {
+                    \\"name\\": \\"foo/bar2/a\\",
+                    \\"path\\": \\"a\\",
+                    \\"isEndpoint\\": false,
+                    \\"file\\": \\"/foo/bar2/a.vue\\"
                   },
-                  Object {
-                    "file": "/o/foo/bar2/b.vue",
-                    "isEndpoint": false,
-                    "name": "foo/bar2/b",
-                    "path": "b",
+                  {
+                    \\"name\\": \\"foo/bar2/b\\",
+                    \\"path\\": \\"b\\",
+                    \\"isEndpoint\\": false,
+                    \\"file\\": \\"/foo/bar2/b.vue\\"
                   },
-                  Object {
-                    "file": "/o/foo/bar2/c.vue",
-                    "isEndpoint": false,
-                    "name": "foo/bar2/c",
-                    "path": "c",
+                  {
+                    \\"name\\": \\"foo/bar2/c\\",
+                    \\"path\\": \\"c\\",
+                    \\"isEndpoint\\": false,
+                    \\"file\\": \\"/foo/bar2/c.vue\\"
                   },
-                ],
-                "file": "/o/foo/bar2/_layout.vue",
-                "isEndpoint": false,
-                "name": "foo/bar2/_layout",
-                "path": "bar2",
+                  {
+                    \\"name\\": \\"foo/bar2/index\\",
+                    \\"path\\": \\"\\",
+                    \\"isEndpoint\\": false,
+                    \\"file\\": \\"/foo/bar2/index.vue\\"
+                  }
+                ]
               },
-              Object {
-                "file": "/o/foo/baz.vue",
-                "isEndpoint": false,
-                "name": "foo/baz",
-                "path": "baz",
-              },
-            ],
-            "file": "/o/foo/_layout.vue",
-            "isEndpoint": false,
-            "name": "foo/_layout",
-            "path": "/foo",
+              {
+                \\"name\\": \\"foo/baz\\",
+                \\"path\\": \\"baz\\",
+                \\"isEndpoint\\": false,
+                \\"file\\": \\"/foo/baz.vue\\"
+              }
+            ]
           },
-          Object {
-            "file": "/o/index.vue",
-            "isEndpoint": false,
-            "name": "index",
-            "path": "/",
-          },
+          {
+            \\"name\\": \\"index\\",
+            \\"path\\": \\"/\\",
+            \\"isEndpoint\\": false,
+            \\"file\\": \\"/index.vue\\"
+          }
         ],
-      }
+        \\"endpoints\\": []
+      }"
     `)
   })
 
   it('handles endpoints', () => {
-    const result = createRoutesLoader(
-      '/o',
-      prepareFs([
-        '/o/index.vue',
-        '/o/_layout.vue',
-        '/o/foo.ts',
-        '/o/foo/bar/index.vue',
-        '/o/foo/bar/_layout.vue',
-        '/o/foo/bar/api.ts',
-      ])
-    ).load()
+    const result = loadRoutes([
+      'index.vue',
+      '_layout.vue',
+      'foo.ts',
+      'foo/bar/index.vue',
+      'foo/bar/_layout.vue',
+      'foo/bar/api.ts',
+    ])
     expect(result).toMatchInlineSnapshot(`
-      Object {
-        "endpoints": Array [
-          Object {
-            "file": "/o/foo/bar/api.ts",
-            "isEndpoint": true,
-            "name": "foo/bar/api",
-            "path": "/foo/bar/api",
-          },
-          Object {
-            "file": "/o/foo.ts",
-            "isEndpoint": true,
-            "name": "foo",
-            "path": "/foo",
-          },
-        ],
-        "errorFile": undefined,
-        "notFoundFile": undefined,
-        "pages": Array [
-          Object {
-            "children": Array [
-              Object {
-                "children": Array [
-                  Object {
-                    "file": "/o/foo/bar/index.vue",
-                    "isEndpoint": false,
-                    "name": "foo/bar/index",
-                    "path": "",
-                  },
-                ],
-                "file": "/o/foo/bar/_layout.vue",
-                "isEndpoint": false,
-                "name": "foo/bar/_layout",
-                "path": "foo/bar",
+      "{
+        \\"pages\\": [
+          {
+            \\"name\\": \\"_layout\\",
+            \\"path\\": \\"/\\",
+            \\"isEndpoint\\": false,
+            \\"file\\": \\"/_layout.vue\\",
+            \\"children\\": [
+              {
+                \\"name\\": \\"foo/bar/_layout\\",
+                \\"path\\": \\"foo/bar\\",
+                \\"isEndpoint\\": false,
+                \\"file\\": \\"/foo/bar/_layout.vue\\",
+                \\"children\\": [
+                  {
+                    \\"name\\": \\"foo/bar/index\\",
+                    \\"path\\": \\"\\",
+                    \\"isEndpoint\\": false,
+                    \\"file\\": \\"/foo/bar/index.vue\\"
+                  }
+                ]
               },
-              Object {
-                "file": "/o/index.vue",
-                "isEndpoint": false,
-                "name": "index",
-                "path": "",
-              },
-            ],
-            "file": "/o/_layout.vue",
-            "isEndpoint": false,
-            "name": "_layout",
-            "path": "/",
-          },
+              {
+                \\"name\\": \\"index\\",
+                \\"path\\": \\"\\",
+                \\"isEndpoint\\": false,
+                \\"file\\": \\"/index.vue\\"
+              }
+            ]
+          }
         ],
-      }
+        \\"endpoints\\": [
+          {
+            \\"name\\": \\"foo/bar/api\\",
+            \\"path\\": \\"/foo/bar/api\\",
+            \\"isEndpoint\\": true,
+            \\"file\\": \\"/foo/bar/api.ts\\"
+          },
+          {
+            \\"name\\": \\"foo\\",
+            \\"path\\": \\"/foo\\",
+            \\"isEndpoint\\": true,
+            \\"file\\": \\"/foo.ts\\"
+          }
+        ]
+      }"
     `)
   })
 })
