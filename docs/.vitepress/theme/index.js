aabab4485e4b2b0841fd545986dc858b6b5e2694
EGOIST <0x142857@gmail.com>
2021-03-30 17:06:40 +0800

    chore(docs): add cloudflare analytics

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,4 +3,19 @@ import DefaultTheme from 'vitepress/theme'
 
 export default {
   ...DefaultTheme,
+  enhanceApp(ctx) {
+    DefaultTheme.enhanceApp && DefaultTheme.enhanceApp(ctx)
+
+    if (!import.meta.env.SSR && import.meta.env.PROD) {
+      const el = document.createElement('script')
+      el.defer = true
+      el.async = true
+      el.src = `https://static.cloudflareinsights.com/beacon.min.js`
+      el.setAttribute(
+        'data-cf-beacon',
+        `{"token": "9d769b3aa3e14c0888f27dc58cc075c7"}`
+      )
+      document.body.append(el)
+    }
+  },
 }
